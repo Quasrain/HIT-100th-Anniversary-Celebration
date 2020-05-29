887f9d92aaa1ad0c66d3bf98985dbb29a3c8bc80
Quasrain <lanceriet@gmail.com>
2020-05-29 17:18:50 +0800

    ADD: 自动审核

--- backend/backendApi/views.py
+++ backend/backendApi/views.py
@@ -1,76 +1,76 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from . import models
+import json
 
 import random
+from aip import AipImageCensor
+
+
+APP_ID = '20127419'
+API_KEY = 'HnCMlNQBzVsQr4z3S57vClIK'
+SECRET_KEY = 'hqpehal5EiZtqs3bjCgoroLpRF9PZwLW'
+client = AipImageCensor(APP_ID, API_KEY, SECRET_KEY)
+
+def check(str):
+    result = client.textCensorUserDefined(str)
+    if result.get('conclusion') == '不合规':
+        return True
+    return False
 
 # 添加评论
 # todo: 自动审核
 def addComment(request):
     comment = models.Comment()
     tmp = request.path.split('/')
-    comment.Name = tmp[2]
-    comment.Tag = tmp[3]
-    comment.Comment = tmp[4]
-    comment.Year = tmp[5]
+    comment.Department = tmp[2]
+    comment.Grade = tmp[3]
+    comment.Identity = tmp[4]
+    comment.Name = tmp[5]
+    comment.Comment = tmp[6]
+    if check(tmp[2]) or check(tmp[3]) or check(tmp[4]) or check(tmp[5]) or check(tmp[6]):
+        data = {
+            'Succ' : False,
+            'UID' : -1,
+        }
+        return JsonResponse(data)
     comment.save()
     ret = comment.UID
-    return HttpResponse('%s' % ret)
+    data = {
+        'Succ' : True,
+        'UID' : ret,
+    }
+    return JsonResponse(data)
 
-# 选择100条评论进行展示，逻辑是top5 + 80条前200高赞，15条随机
+# 选择第x页，每页100条
 def selectComment(request):
-    decades = [0 for i in range(10)]
-    data = models.Comment.objects.all().order_by('-Year')
-
-    if len(data) <= 100:
-        ret = []
-        for x in data:
-            tmp = [x.UID, x.Name, x.Tag, x.Comment, x.Year]
-            ret.append(tmp)
-        return HttpResponse(ret)
-
-    data_top = models.Comment.objects.all().order_by('-Count')
-    select_uid = set()
-    for i in range(5):
-        year = data_top[i].Year
-        year = (year - 1920) / 10
-        decades[year] += 1
-        select_uid.add(data_top[i].UID)
+    data = models.Comment.objects.all()
+    tmp = request.path.split('/')
+    x = int(tmp[2])
+    if (x <= 0):
+        left = 0
+        right = len(data)
+    else:
+        left = (x - 1) * 100
+        right = min(x * 100, len(data))
+    data = data[left : right]
     ret = []
-    cnt = 0
-    while len(select_uid) < 85:
-        cnt += 1
-        y = 0
-        if len(data_top) > 200:
-            y = 199
-        else:
-            y = len(data_top) - 1 
-        x = random.randint(0, y)
-        year = data_top[x].Year
-        year = (year - 1920) / 10
-        if year == 10:
-            year = 9
-        if decades[year] >= 10 or data_top[x].UID in select_uid:
-            if cnt < 500:
-                continue
-        decades[year] += 1
-        select_uid.add(data_top[x].UID)
-    while len(select_uid) < 100:
-        x = random.randint(1, len(data_top) - 1)
-        year = data_top[x].Year
-        year = (year - 1920) / 10
-        if year == 10:
-            year = 9
-        if decades[year] >= 10 or data_top[x].UID in select_uid:
-            if cnt < 500:
-                continue
-        decades[year] += 1
-        select_uid.add(data_top[x].UID)
-    for x in select_uid:
-        tmp = models.Comment.objects.get(UID = x)
-        ret.append([tmp.UID, tmp.Name, tmp.Tag, tmp.Comment, tmp.Year])
-    res = sorted(ret, key = lambda x: x[4], reverse = True)
-    return HttpResponse(res)
+    for x in data:
+        tmp = {
+            'UID' : x.UID,     
+            'Department' : x.Department,
+            'Grade' : x.Grade,
+            'Identity' : x.Identity,
+            'Name' : x.Name,
+            'Comment': x.Comment,
+            'Count': x.Count,
+        }
+        ret.append(tmp)
+    res = {
+        'size' : len(data),
+        'data' : ret,
+    }
+    return JsonResponse(res, safe = False, json_dumps_params={'ensure_ascii':False})
 
 # 点赞功能，需要前端传输UID
 def like(request):
@@ -78,7 +78,10 @@ def like(request):
     uid = tmp[2]
     count = models.Comment.objects.get(UID = uid)
     models.Comment.objects.filter(UID = uid).update(Count = count.Count + 1)
-    return HttpResponse(count.Count + 1)
+    data = {
+        'Count' : count.Count + 1,
+    }
+    return JsonResponse(data)
 
 
 # 抽取最后的幸运参与者，不急
@@ -96,4 +99,20 @@ def lottery(request):
             Set.add(x)
             res.append([data_top[x].UID, data_top[x].Name, data_top[x].Tag, data_top[x].Comment, data_top[x].Count])
             cnt += 1
-    return HttpResponse(res)
+    res = sorted(res, key = lambda x: x[4], reverse = True)
+    ret = []
+    for x in res:
+        data = {
+            'UID' : x[0],
+            'Name' : x[1],
+            'Tag' : x[2],
+            'Comment': x[3],
+        }
+        ret.append(data)
+    data = {
+        'data' : ret,
+    }
+    return JsonResponse(data, safe = False, json_dumps_params={'ensure_ascii':False})
+
+
+# 选取
